1edef8d58afec79c0508c54d88b5cbda27f0d9a6
Holger Hees <holger.hees@gmail.com>
2018-09-27 12:04:03 +0200

    changed jython path

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ echo Launching the openHAB runtime...
 DIRNAME=`dirname "$0"`
 
 #export JAVA_HOME="/dataDisk/java/"
-export JAVA_OPTS="${JAVA_OPTS} -Dgnu.io.rxtx.SerialPorts=/dev/ttyOpenHabLueftung:/dev/ttyOpenHabFunk -Xbootclasspath/a:/dataDisk/jython/jython.jar -Dpython.home=/dataDisk/jython -Dpython.path=/dataDisk/openhab-runtime/python"
+export JAVA_OPTS="${JAVA_OPTS} -Dgnu.io.rxtx.SerialPorts=/dev/ttyOpenHabLueftung:/dev/ttyOpenHabFunk -Xbootclasspath/a:/opt/jython/dist/jython.jar -Dpython.home=/opt/jython/dist -Dpython.path=/dataDisk/openhab-runtime/python"
 #export JYTHONPATH="${DIRNAME}/python"
 export OPENHAB_HOME="${DIRNAME}"
 export OPENHAB_CONF="${DIRNAME}/conf"
