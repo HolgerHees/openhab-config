b5e304a686a30b32c1a42e071bdbbcc740d6da86
Holger Hees <holger.hees@gmail.com>
2018-09-30 14:21:41 +0200

    charset fix

--- conf/automation/jsr223/door_bell_notification.py
+++ conf/automation/jsr223/door_bell_notification.py
@@ -9,7 +9,7 @@ class DoorBellNotificationRule:
 
     def execute(self, module, input):
         if itemStateOlderThen("Bell_Last_Change", getNow().minusSeconds(30)):
-            sendNotification("Klingel", "Es klingelt", "/cameraStrasseImage")
-            sendMail("Es klingelt", u"Es klingelt jemand an der Tür", "/cameraStrasseImage")
+            sendNotification("Klingel", "Es klingelt", "https://smartmarvin.de/cameraStrasseImage")
+            sendMail("Es klingelt", u"Es klingelt jemand an der Tür", "https://smartmarvin.de/cameraStrasseImage")
 
         postUpdate("Bell_Last_Change", DateTimeType())
