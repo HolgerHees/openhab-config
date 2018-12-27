40cbc015fa13ddf1652e6e3cd6fc1d32e4671aa2
Holger Hees <holger.hees@gmail.com>
2018-12-27 22:04:16 +0100

    fix bassbox rule

--- conf/automation/jsr223/receiver_control.py
+++ conf/automation/jsr223/receiver_control.py
@@ -1,4 +1,4 @@
-from marvin.helper import rule, postUpdate
+from marvin.helper import rule, sendCommand
 from core.triggers import ItemStateChangeTrigger
 
 @rule("receiver_control.py")
@@ -8,6 +8,6 @@ class ReceiverControlRule:
 
     def execute(self, module, input):
         if input["newState"] == OPEN:
-            postUpdate("Socket_Livingroom_Bassbox", ON)
+            sendCommand("Socket_Livingroom_Bassbox", ON)
         else:
-            postUpdate("Socket_Livingroom_Bassbox", OFF)
+            sendCommand("Socket_Livingroom_Bassbox", OFF)
