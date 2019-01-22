2dc31d12a38ff5388ff993e598bec34b4d033723
Holger Hees <holger.hees@gmail.com>
2019-01-22 20:13:53 +0100

    improved presence detection

--- conf/automation/jsr223/presence_detection.py
+++ conf/automation/jsr223/presence_detection.py
@@ -1,4 +1,4 @@
-from marvin.helper import log, rule, createTimer, itemLastUpdateOlderThen, itemLastUpdateNewerThen, getNow, getFilteredChildItems, getGroupMember, getItemLastUpdate, getItemState, postUpdate, postUpdateIfChanged, sendCommand, sendNotification
+from marvin.helper import log, rule, itemLastUpdateOlderThen, getNow, getItemState, postUpdate, sendNotification
 from core.triggers import ItemStateChangeTrigger
 
 
@@ -20,29 +20,18 @@ class PresenceCheckRule:
         sandraPhone = itemState if itemName == "State_Sandra_Presence" else getItemState("State_Sandra_Presence")
         
         if holgerPhone == ON or sandraPhone == ON:
+            # only possible if we are away
             if getItemState("State_Presence").intValue() == 0:
                 postUpdate("State_Presence",1)
                 sendNotification(u"Tür", u"Willkommen")
         else:
-            if getItemState("State_Presence").intValue() != 0:
+            # only possible if we are present and not sleeping
+            if getItemState("State_Presence").intValue() == 1:
                 postUpdate("State_Presence",0)
                 lightMsg = u" - LICHT an" if getItemState("Lights_Indoor") != OFF else u""
                 windowMsg = u" - FENSTER offen" if getItemState("Openingcontacts") != CLOSED else u""
 
                 sendNotification(u"Tür", u"Auf Wiedersehen{}{}".format(lightMsg,windowMsg))
-
-#@rule("presence_detection.py")
-#class UnexpectedMotionRule:
-#    def __init__(self):
-#        self.triggers = [
-#            ItemStateChangeTrigger("Motiondetector_FF_Floor",state="OPEN"),
-#            ItemStateChangeTrigger("Motiondetector_FF_Livingroom",state="OPEN"),
-#            ItemStateChangeTrigger("Motiondetector_SF_Floor",state="OPEN")
-#        ]
-
-#    def execute(self, module, input):
-#        if getItemState("State_Presence").intValue() == 0:
-#            sendNotification(u"Unexpected Motion", u"{}".format(input['event'].getItemName()))
 
 @rule("presence_detection.py")
 class WakeupRule:
@@ -56,11 +45,13 @@ class WakeupRule:
         ]
 
     def execute(self, module, input):        
+        # only possible if we are sleeping
         if getItemState("State_Presence").intValue() == 2:
             # sometimes the "Lights_FF" state switches back and forth for a couple of milliseconds when set "Lights_FF" state to OFF
             if itemLastUpdateOlderThen("State_Presence",getNow().minusSeconds(5)):
-                if getItemState("TV_Online") == ON or getItemState("Lights_FF") == ON or getItemState("Shutters_FF") == PercentType.ZERO:
+                if getItemState("Lights_FF") == ON or getItemState("Shutters_FF") == PercentType.ZERO:
                     postUpdate("State_Presence", 1)
+                    sendNotification(u"System", u"Guten Morgen")
 
 @rule("presence_detection.py") 
 class SleepingRule:
@@ -68,6 +59,7 @@ class SleepingRule:
         self.triggers = [ ItemStateChangeTrigger("Scene4",state="ON") ]
 
     def execute(self, module, input):
+        # only possible if we are present
         if getItemState("State_Presence").intValue() == 1:
             postUpdate("State_Presence", 2)
             
