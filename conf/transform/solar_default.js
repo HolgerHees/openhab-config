70665e31935bd27bc64b3c8aebd5abdf79d15a07
Holger Hees <holger.hees@gmail.com>
2019-04-23 21:47:04 +0200

    fixed photovoltaic values

---
+++ conf/transform/solar_default.js
@@ -0,0 +1,4 @@
+(function(i) {
+    if( i == "-2147483648" ) return 0;
+    return parseInt(i);
+})(input)
--- conf/transform/solar_fixed2.js
+++ conf/transform/solar_fixed2.js
@@ -1,3 +1,4 @@
 (function(i) {
+    if( i == "-2147483648" ) return 0.0;
     return parseFloat(i) / 100.0;
 })(input)
