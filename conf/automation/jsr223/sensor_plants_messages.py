25585e14a767acfae8236b76d5ee145ac7aa241b
Holger Hees <holger.hees@gmail.com>
2019-05-19 14:22:23 +0200

    improved plant sensor handling

--- conf/automation/jsr223/sensor_plants_messages.py
+++ conf/automation/jsr223/sensor_plants_messages.py
@@ -32,10 +32,10 @@ class DetailRule:
         msg = u"Nicht aktiv"
 
         if getItemState("Auto_Attic_Light").intValue() != 1:
-            soilMoistSensor1 = getItemState("SoilMoistSensor1").intValue()
-            soilMoistSensor2 = getItemState("SoilMoistSensor2").intValue()
-            soilMoistSensor3 = getItemState("SoilMoistSensor3").intValue()
-            soilMoistSensor4 = getItemState("SoilMoistSensor4").intValue()
+            soilMoistSensor1 = getItemState("SoilMoistSensor1").intValue() if getItemState("SoilMoistSensor1Enabled") == ON else 1000
+            soilMoistSensor2 = getItemState("SoilMoistSensor2").intValue() if getItemState("SoilMoistSensor2Enabled") == ON else 1000
+            soilMoistSensor3 = getItemState("SoilMoistSensor3").intValue() if getItemState("SoilMoistSensor3Enabled") == ON else 1000
+            soilMoistSensor4 = getItemState("SoilMoistSensor4").intValue() if getItemState("SoilMoistSensor4Enabled") == ON else 1000
 
             if soilMoistSensor1 < 380 or soilMoistSensor2 < 380 or soilMoistSensor3 < 380 or soilMoistSensor4 < 380:
                 msg = u"Jetzt Giessen"
@@ -45,3 +45,32 @@ class DetailRule:
                 msg = u"Feucht genug"
 
         postUpdateIfChanged("PlantSensorInfo", msg)
+
+@rule("scene_plant_messages.py")
+class EnabledRule:
+    def __init__(self):
+        self.triggers = [
+            ItemStateChangeTrigger("SoilMoistSensor1Enabled"),
+            ItemStateChangeTrigger("SoilMoistSensor2Enabled"),
+            ItemStateChangeTrigger("SoilMoistSensor3Enabled"),
+            ItemStateChangeTrigger("SoilMoistSensor4Enabled"),
+            ItemStateChangeTrigger("SoilMoistSensor5Enabled"),
+            ItemStateChangeTrigger("SoilMoistSensor6Enabled")
+        ]
+
+    def execute(self, module, input):
+        active = []
+        if getItemState("SoilMoistSensor1Enabled") == ON:
+            active.append("1")
+        if getItemState("SoilMoistSensor2Enabled") == ON:
+            active.append("2")
+        if getItemState("SoilMoistSensor3Enabled") == ON:
+            active.append("3")
+        if getItemState("SoilMoistSensor4Enabled") == ON:
+            active.append("4")
+        if getItemState("SoilMoistSensor5Enabled") == ON:
+            active.append("5")
+        if getItemState("SoilMoistSensor6Enabled") == ON:
+            active.append("6")
+        postUpdateIfChanged("SoilMoistSensorInfo", u", ".join(active)) 
+            
