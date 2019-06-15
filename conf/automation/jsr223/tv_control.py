5fa9370b561bc186fd6858f6820d8ad2e73b2149
Holger Hees <holger.hees@gmail.com>
2019-06-15 14:56:27 +0200

    more samsung tv related changes

---
+++ conf/automation/jsr223/tv_control.py
@@ -0,0 +1,22 @@
+from marvin.helper import rule, getItemState, postUpdate, postUpdateIfChanged
+from core.triggers import ItemStateChangeTrigger, ItemCommandTrigger, ThingEventTrigger
+
+@rule("tv_control.py")
+class TvLivingroomStatusRule:
+    def __init__(self):
+        self.triggers = [ ThingEventTrigger("samsungtv:tv:livingroom", "ThingStatusInfoChangedEvent") ]
+
+    def execute(self, module, input):
+        postUpdate("TV_KEY_POWER", ON if input["event"].getStatusInfo().getStatus().toString() == "ONLINE" else OFF )
+
+@rule("tv_control.py")
+class TvLivingroomControlRule:
+    def __init__(self):
+        self.triggers = [ ItemCommandTrigger("TV_KEY_POWER") ]
+
+    def execute(self, module, input):
+        if input["event"].getItemCommand() == ON:
+            sendCommand("TV_KEY_POWER_ON",ON)
+        else:
+            pass
+        #postUpdate("TV_KEY_POWER", ON if input["event"].getStatusInfo() == ONLINE else OFF )
