1469f2d4500dbe2333eddec74f3d5d8a99367be7
Holger Hees <holger.hees@gmail.com>
2019-06-23 16:45:18 +0200

    changed knx switches in bedroom

--- conf/automation/jsr223/lights_indoor_bedroom.py
+++ conf/automation/jsr223/lights_indoor_bedroom.py
@@ -1,5 +1,5 @@
-from marvin.helper import rule, sendCommand
-from core.triggers import ItemStateChangeTrigger
+from marvin.helper import rule, sendCommand, getItemState
+from core.triggers import ItemStateChangeTrigger, ItemStateUpdateTrigger
 
 
 @rule("lights_indoor_bedroom_control.py")
@@ -12,3 +12,29 @@ class LightsIndoorBedroomControlRule:
 
     def execute(self, module, input):
         sendCommand("Scene4", ON)
+
+@rule("lights_indoor_bedroom_left_control.py")
+class LightsIndoorBedroomLeftControlRule:
+    def __init__(self):
+        self.triggers = [
+            ItemStateUpdateTrigger("Light_SF_Bedroom_Left", state="ON")
+        ]
+
+    def execute(self, module, input):
+        if getItemState("Light_SF_Bedroom_Left_Hue_Brightness").intValue() == 0:
+            sendCommand("Light_SF_Bedroom_Left_Hue_Brightness",100)
+        else:
+            sendCommand("Light_SF_Bedroom_Left_Hue_Brightness",0)
+            
+@rule("lights_indoor_bedroom_right_control.py")
+class LightsIndoorBedroomRightControlRule:
+    def __init__(self):
+        self.triggers = [
+            ItemStateUpdateTrigger("Light_SF_Bedroom_Right", state="ON")
+        ]
+
+    def execute(self, module, input):
+        if getItemState("Light_SF_Bedroom_Right_Hue_Brightness").intValue() == 0:
+            sendCommand("Light_SF_Bedroom_Right_Hue_Brightness",100)
+        else:
+            sendCommand("Light_SF_Bedroom_Right_Hue_Brightness",0)
