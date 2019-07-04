8c148ca0dc72b58ec50773160ed32965880e96cc
Holger Hees <holger.hees@gmail.com>
2019-07-04 20:18:08 +0200

    changed svg icons

--- svg/openhab/convert.sh
+++ svg/openhab/convert.sh
@@ -5,9 +5,16 @@ DIRNAME=`dirname "$0"`
 cd $DIRNAME
 
 rm png/*.png
+#rm svg.optimized/*.svg
 rm ../../conf/icons/classic/*.png
+rm ../../conf/icons/classic/*.svg
 
-mogrify -background "#00000000" -path png/ -format png new/*.svg
+mogrify -background "#00000000" -path png/ -format png svg/*.svg
+
+#npm install -g svgo
+#svgo svg/*.svg -o svg.optimized/
 
 cp png/*.png ../../conf/icons/classic/
+#cp svg/*.svg ../../conf/icons/classic/
+#cp svg.optimized/*.svg ../../conf/icons/classic/
 
