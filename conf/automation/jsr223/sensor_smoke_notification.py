22524822dfed81c82144ca10cca4308d7b5e5f3c
holger.hees@gmail.com <holger.hees@gmail.com>
2020-04-06 14:41:10 +0200

    fix smoke detector notification

--- conf/automation/jsr223/sensor_smoke_notification.py
+++ conf/automation/jsr223/sensor_smoke_notification.py
@@ -4,7 +4,7 @@ from custom.helper import rule, ItemStateChangeTrigger, sendNotification
 @rule("scene_contact_notifications.py")
 class SmokeDetectorNotificationsRule:
     def __init__(self):
-        self.triggers = [ItemStateChangeTrigger("Smoke_Detector",state="ON")]
+        self.triggers = [ItemStateChangeTrigger("Smoke_Detector",state="OPEN")]
 
     def execute(self, module, input):
         sendNotification("Alarm", u"Rauchmelder") 
