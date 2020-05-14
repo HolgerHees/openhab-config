e4abddaf62526df4e3f1959bd2f596329b85fc15
holger.hees@gmail.com <holger.hees@gmail.com>
2020-05-14 19:43:54 +0200

    improved wattering rule

--- conf/automation/jsr223/scenes_wathering.py
+++ conf/automation/jsr223/scenes_wathering.py
@@ -1,19 +1,63 @@
 import math
 
 from custom.helper import rule, createTimer, getNow, getGroupMember, getItemState, postUpdate, sendCommand, getItemLastUpdate
-from core.triggers import ItemCommandTrigger
+from core.triggers import ItemCommandTrigger, ItemStateChangeTrigger
 
 circuits = [
     [
-        ['Watering_Streetside_Lawn', u"Strasse", 2.0 / 3.0],
-        ['Watering_Gardenside_Lawn_right', u"Garten rechts", 1.00],
-        ['Watering_Gardenside_Lawn_left', u"Garten links", 1.00]
+        ['Watering_Streetside_Lawn', u"Strasse", 2.0 / 3.0 ],
+        ['Watering_Gardenside_Lawn_right', u"Garten rechts", 1.0 ],
+        ['Watering_Gardenside_Lawn_left', u"Garten links", 1.0 ]
     ], [
-        ['Watering_Streetside_Beds']
+        ['Watering_Streetside_Beds', u"Beete", 1.0 ]
     ], [
-        ['Watering_Gardenside_Beds_front']
+        ['Watering_Gardenside_Beds_front', u"Beete", 1.0 ]
     ]
 ]
+
+@rule("scenes_wathering.py")
+class ScenesWatheringRule:
+    def __init__(self):
+        self.triggers = [
+            ItemStateChangeTrigger("Watering_Streetside_Lawn_Auto"),
+            ItemStateChangeTrigger("Watering_Gardenside_Lawn_right_Auto"),
+            ItemStateChangeTrigger("Watering_Gardenside_Lawn_left_Auto"),
+            ItemStateChangeTrigger("Watering_Gardenside_Beds_front_Auto"),
+            ItemStateChangeTrigger("Watering_Streetside_Beds_Auto"),
+            ItemStateChangeTrigger("Watering_Gardenside_Beds_back_Auto"),
+            ItemStateChangeTrigger("Watering_Program_Duration")
+        ]
+
+    def execute(self, module, input):
+        reference_duration = getItemState("Watering_Program_Duration").intValue()
+
+        total = 0
+        for loop in circuits[0]:            
+            if getItemState(loop[0] + "_Auto") == ON:
+                duration = int( math.floor( loop[2] * reference_duration ) )
+                total = total + duration
+            
+                postUpdate(loop[0] + "_Info", u"{} min.".format(duration))
+            else:
+                postUpdate(loop[0] + "_Info", u"inaktiv")
+                
+        if total == 0:
+            total = reference_duration
+        
+        if getItemState("Watering_Gardenside_Beds_front_Auto") == ON:
+            postUpdate("Watering_Gardenside_Beds_front_Info", u"{} min.".format(total))
+        else:
+            postUpdate("Watering_Gardenside_Beds_front_Info", u"inaktiv")
+        
+        if getItemState("Watering_Streetside_Beds_Auto") == ON:
+            postUpdate("Watering_Streetside_Beds_Info", u"{} min.".format(total))
+        else:
+            postUpdate("Watering_Streetside_Beds_Info", u"inaktiv")
+
+        if getItemState("Watering_Gardenside_Beds_back_Auto") == ON:
+            postUpdate("Watering_Gardenside_Beds_back_Info", u"{} min.".format(total))
+        else:
+            postUpdate("Watering_Gardenside_Beds_back_Info", u"inaktiv")
 
 @rule("scenes_wathering.py")
 class ScenesWatheringRule:
@@ -41,43 +85,61 @@ class ScenesWatheringRule:
         duration = getItemState("Watering_Program_Duration").intValue() * 60.0 * 1000.0
         
         remaining = 0
-        activeStep = None
-
+        info = u""
+            
         if getItemState("Watering_Circuits") == OFF:
-            for loop in circuits:
+            for group in circuits:
                 #self.log.info("start " + loop[0][0])
-                sendCommand(loop[0][0], ON)
-            activeStep = circuits[0][0]
-            remaining = ( duration * activeStep[2] )
+                for circuit in group:
+                    if getItemState(circuit[0] + "_Auto") == ON:
+                        sendCommand(circuit[0], ON)
+
+                        if remaining == 0:
+                            remaining = ( duration * circuit[2] )
+                            info = circuit[1]
+                        break
+                      
         else:
+            referenceGroup = -1
+            for i in range(len(circuits)):
+                for circuit in circuits[i]:
+                    if getItemState(circuit[0] + "_Auto") == ON:
+                        referenceGroup = i
+                        break
+                      
+                if referenceGroup != -1:
+                    break
+                    
             activeIndex = -1
-            for i in range(len(circuits[0])):
-                step = circuits[0][i]
+            activeStep = None
+            for i in range(len(circuits[referenceGroup])):
+                step = circuits[referenceGroup][i]
+                
                 if getItemState(step[0]) == ON:
                     activeIndex = i
                     activeStep = step
                     break
-            
+
             if activeStep != None:
                 runtime = getNow().getMillis() - getItemLastUpdate(activeStep[0]).getMillis()
                 
                 remaining = ( duration * activeStep[2] ) - runtime
-                
                 if remaining <= 0:
                     activeIndex += 1
-                    if activeIndex < len(circuits[0]):
-                        #self.log.info("next " + circuits[0][activeIndex][0])
-                        sendCommand(circuits[0][activeIndex][0], ON)
+                    if activeIndex < len(circuits[referenceGroup]):
+                        sendCommand(circuits[referenceGroup][activeIndex][0], ON)
                         sendCommand(activeStep[0], OFF)
-                        activeStep = circuits[0][activeIndex]
+
+                        activeStep = circuits[referenceGroup][activeIndex]
+
                         remaining = ( duration * activeStep[2] )
+                        info = activeStep[1]
                     else:
-                        activeStep = None
                         #self.log.info("finish")
                         self.disableAllCircuits()
                         postUpdate("Watering_Program_Start", OFF)
-            
-        return [ activeStep[1] if activeStep != None else u"", remaining ]
+
+        return [ info, remaining ]
 
     def callbackProgress(self):
         if self.progressTimer is not None and self.currentProgressMsg != getItemState("Watering_Program_State").toString():
