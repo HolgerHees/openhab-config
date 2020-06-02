8be9121a381fe9c34929d4790d886166a7e3f3b2
holger.hees@gmail.com <holger.hees@gmail.com>
2020-06-02 10:34:41 +0200

    fixed jython setup

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ echo Launching the openHAB runtime...
 DIRNAME=`dirname "$0"`
 
 #export JAVA_HOME="/dataDisk/java/"
-export JAVA_OPTS="${JAVA_OPTS} -Dgnu.io.rxtx.SerialPorts=/dev/ttyOpenHabLueftung:/dev/ttyOpenHabFunk -Xbootclasspath/a:${DIRNAME}/runtime/jython.jar -Dpython.home=${DIRNAME}/runtime -Dpython.path=${DIRNAME}/python"
+export JAVA_OPTS="${JAVA_OPTS} -Dgnu.io.rxtx.SerialPorts=/dev/ttyOpenHabLueftung:/dev/ttyOpenHabFunk -Xbootclasspath/a:${DIRNAME}/runtime/lib/jython.jar -Dpython.home=${DIRNAME}/runtime/lib/ -Dpython.path=${DIRNAME}/python"
 #export JYTHONPATH="${DIRNAME}/python"
 export OPENHAB_HOME="${DIRNAME}"
 export OPENHAB_CONF="${DIRNAME}/conf"
