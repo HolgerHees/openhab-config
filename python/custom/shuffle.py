05811368e5cc80793244fbb69fdf305c2ee86f98
Holger Hees <holger.hees@gmail.com>
2023-06-30 21:03:23 +0200

    fix pluar form of random generator

--- python/custom/shuffle.py
+++ python/custom/shuffle.py
@@ -28,10 +28,10 @@ class ShuffleHelper:
                 u"Willkommen zu Hause",
                 u"Willkommen zurück",
                 u"Herzlich willkommen daheim",
-                u"Schön, dass du wieder zu Hause bist",
-                u"Schön das du wieder da bist",
-                u"Schön das du zurück bist",
-                u"Schön dich zu sehen"
+                { False: u"Schön, dass du wieder zu Hause bist", True: u"Schön, dass ihr wieder zu Hause seid" },
+                { False: u"Schön das du wieder da bist", True: u"Schön, dass ihr wieder da seid" },
+                { False: u"Schön das du zurück bist", True: u"Schön, dass ihr wieder zurück seid" },
+                { False: u"Schön dich zu sehen", True: u"Schön, euch zu sehen" }
             ],
             [
                 u"",
@@ -39,22 +39,26 @@ class ShuffleHelper:
                 u"",
                 u"",
                 u"Ich hab mich schon gelangweilt",
-                u"War ganzschön langweilig ohne dich",
-                u"Ich hoffe du hattest einen angenehmen Tag"
+                { False: u"War ganzschön langweilig ohne dich", True: u"War ganzschön langweilig ohne euch" },
+                { False: u"Ich hoffe du hattest einen angenehmen Tag", True: u"Ich hoffe ihr hattet einen angenehmen Tag" }
             ]
         ]
     }
 
     @staticmethod
-    def getRandomSynonym(input):
+    def getRandomSynonym(input, flag = None):
         if input in ShuffleHelper._synonyms:
             values = ShuffleHelper._synonyms[input]
             if type(values[0]) is list:
                 parts = []
                 for _values in values:
-                    parts.append(random.choice(_values))
-                return ". ".join(parts)
+                    choice = random.choice(_values)
+                    if type(choice) is dict:
+                        parts.append(choice[flag])
+                    elif len(choice) > 0:
+                        parts.append(choice)
+                return u"{}.".format( u". ".join(parts) )
             else:
-                return random.choice(ShuffleHelper._synonyms[input])
+                return u"{}.".format( random.choice(ShuffleHelper._synonyms[input]) )
         return input
 
