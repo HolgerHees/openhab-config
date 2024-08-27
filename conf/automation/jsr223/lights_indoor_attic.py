a291977aee79f74a26c15d420ce3cf78d84ef970
HolgerHees <holger.hees@gmail.com>
2024-08-27 14:14:04 +0200

    improved samsung tv handling

--- conf/automation/jsr223/lights_indoor_attic.py
+++ conf/automation/jsr223/lights_indoor_attic.py
@@ -1,6 +1,6 @@
 from java.time import ZonedDateTime
 
-from shared.helper import rule, getItemState, sendCommand
+from shared.helper import rule, getItemState, sendCommandIfChanged
 from shared.triggers import CronTrigger, ItemStateChangeTrigger
 
 
@@ -8,22 +8,20 @@ from shared.triggers import CronTrigger, ItemStateChangeTrigger
 class LightsIndoorAttic:
     def __init__(self):
         self.triggers = [
-            CronTrigger("0 1 5,8,20,23 * * ?"),
+            CronTrigger("0 1 * * * ?"),
             ItemStateChangeTrigger("pOther_Manual_State_Auto_Attic_Light")
         ]
 
     def execute(self, module, input):
+        state = getItemState("pOther_Manual_State_Auto_Attic_Light").intValue();
+        if state not in [2,3]:
+            return
+
         hour = ZonedDateTime.now().getHour()
 
-        state = getItemState("pOther_Manual_State_Auto_Attic_Light").intValue();
-        
         if state == 2:
-            if hour == 5:
-                sendCommand("pMobile_Socket_7_Powered", ON)
-            elif hour == 23:
-                sendCommand("pMobile_Socket_7_Powered", OFF)
-        elif state == 3:
-            if hour == 8:
-                sendCommand("pMobile_Socket_7_Powered", ON)
-            elif hour == 20:
-                sendCommand("pMobile_Socket_7_Powered", OFF)
+            state = ON if hour >= 5 and hour <= 23 else OFF
+        else:
+            state = ON if hour >= 8 and hour <= 20 else OFF
+
+        sendCommandIfChanged("pMobile_Socket_7_Powered", state)
--- conf/automation/jsr223/tv_control.py
+++ conf/automation/jsr223/tv_control.py
@@ -1,23 +1,29 @@
 from shared.helper import rule, sendCommand, postUpdate
-from shared.triggers import ItemCommandTrigger, ThingEventTrigger
+from shared.triggers import ItemCommandTrigger, ItemStateChangeTrigger, ThingEventTrigger
 
 
 @rule()
-class TvControlLivingroomStatus:
+class TvControlLivingroomPowerState:
     def __init__(self):
-        self.triggers = [ ThingEventTrigger("samsungtv:tv:livingroom", "ThingStatusInfoChangedEvent") ]
+        self.triggers = [
+            ItemStateChangeTrigger("pGF_Livingroom_Television_Key_POWER")
+        ]
 
     def execute(self, module, input):
-        postUpdate("pGF_Livingroom_Television_Key_POWER", ON if input["event"].getStatusInfo().getStatus().toString() == "ONLINE" else OFF )
+        if input["event"].getItemState() == ON:
+            postUpdate("pGF_Livingroom_Television_POWER",ON)
+        else:
+            postUpdate("pGF_Livingroom_Television_POWER",OFF)
 
 @rule()
 class TvControlLivingroomControl:
     def __init__(self):
-        self.triggers = [ ItemCommandTrigger("pGF_Livingroom_Television_Key_POWER") ]
+        self.triggers = [ ItemCommandTrigger("pGF_Livingroom_Television_POWER") ]
 
     def execute(self, module, input):
         if input["event"].getItemCommand() == ON:
-            sendCommand("pGF_Livingroom_Television_Key_POWER_ON",ON)
-        #else:
-        #    sendCommand("pGF_Livingroom_Television_Key_POWER_OFF",ON)
-        #postUpdate("pGF_Livingroom_Television_Key_POWER", ON if input["event"].getStatusInfo() == ONLINE else OFF )
+            sendCommand("pGF_Livingroom_SkyQ_Key_POWER",ON)
+            sendCommand("pGF_Livingroom_Television_Key_POWER",ON)
+        else:
+            sendCommand("pGF_Livingroom_SkyQ_Key_POWER",OFF)
+            sendCommand("pGF_Livingroom_Television_Key_POWER",OFF)
